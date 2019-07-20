d5c6672f7ac79843d827356c67496cecfd0f4ac3
wathigo <wathigosimon@gmail.com>
2019-07-20 04:24:32 -0400

    Add a sign out button to the nav bar Add more styling to the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from '../images/students.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = () => {
   return (
@@ -7,7 +9,9 @@ const Nav = () => {
       <figure className="fl-left logo">
         <img src={logo} alt="log0"/>
       </figure>
+      <span className="cl-white nav-header"> Logged in as Admin: k09-5039-2783 </span>
       <input type="text" placeholder="search" className="fl-right"/>
+      <span className='power'> <FontAwesomeIcon className="power-btn" icon={faPowerOff}/></span>
     </div>
   );
 }
